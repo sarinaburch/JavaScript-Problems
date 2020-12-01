aafae5eb5fc3083bc457674e6eb5ed0cca49cfec
Sarina Burch <sarinaburch52@gmail.com>
2020-12-01 14:12:48 -0500

    basic functions, adding arguments, returning values in console.

---
+++ Exercise Files/arguments/script.js
@@ -0,0 +1,12 @@
+function findBiggestFraction(a, b) {
+    a>b ? console.log("a:", a) : console.log("b: ", b);
+    console.log("The function is running!");
+}
+
+// var a = 3/4;
+// var b = 5/7;
+
+var firstFraction = 7/16;
+var secondFraction = 12/25;
+
+findBiggestFraction(firstFraction, secondFraction);
