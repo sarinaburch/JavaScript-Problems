628dc33770780c597172ee9b72f1d50faa5d1dc3
Sarina Burch <sarinaburch52@gmail.com>
2020-12-01 16:59:20 -0500

    resolved problem with second hand resetting after 360, fully functional

--- analogClock/script.js
+++ analogClock/script.js
@@ -2,24 +2,28 @@ const HOURHAND = document.querySelector("#hour");
 const MINUTEHAND = document.querySelector("#minute");
 const SECONDHAND = document.querySelector("#second");
 
-function runTheClock() {
-
     //adding current hour, minute, sec
     var date = new Date();
     let hr = date.getHours();
     let min = date.getMinutes();
     let sec = date.getSeconds();
 
-    //converting Date to coresspond with current time 
+    //converting Date to correspond with current time 
     let hrPosition = (hr*360/12) + (min*(360/60)/12);
     let minPosition = (min*360/60) + (sec*(360/60)/60);
     let secPosition = sec*360/60;
 
+function runTheClock() {
+    
+    secPosition = secPosition + 6;
+    minPosition = minPosition + (6/60);
+    hrPosition = hrPosition + (3/360);
 
-HOURHAND.style.transform = "rotate(" + hrPosition + "deg)";
-MINUTEHAND.style.transform = "rotate(" + minPosition + "deg)";
-SECONDHAND.style.transform = "rotate(" + secPosition + "deg)";
+    HOURHAND.style.transform = "rotate(" + hrPosition + "deg)";
+    MINUTEHAND.style.transform = "rotate(" + minPosition + "deg)";
+    SECONDHAND.style.transform = "rotate(" + secPosition + "deg)";
 }
 
+//moves hands around along with current time
 var interval = setInterval(runTheClock, 1000);
 
